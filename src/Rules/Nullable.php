22e81641e8af2b2ce478e556da7be30d76c882a1
gdarko <dg@darkog.com>
2020-06-30 00:30:16 +0000

    Added nullable rule
    Thanks to @validation

---
+++ src/Rules/Nullable.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace DG\Validation\Rules;
+
+
+use DG\Validation\Rule;
+
+class Nullable extends Rule
+{
+    /**
+     * Check the $value is valid
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    public function check($value): bool
+    {
+        return true;
+    }
+}
